9e113ed7d734b62b176e0c522fe516df1aac1fac
Andrew Wiik <andrew.wiik@vtsd.com>
2016-10-27 07:34:56 -0700

    Update genlist.js

--- genlist.js
+++ genlist.js
@@ -7,14 +7,12 @@ var x;
 var y;
 var z;
 
-for (w =0; w < 3; w++) {
-    for (x = 0; x < 3; x++) {
-        for (y = 1; y < 5; y++) {
+    for (x = 0; x < 26; x++) {
+        for (y = 1; y < 10; y++) {
             for (z = 0; z < 26; z++) {
-                logger.write('NP3C'+ String.fromCharCode(65 + x) + '0' + y + String.fromCharCode(65 + z) + 'A' + '\n');
+                logger.write('FP3Y'+ String.fromCharCode(65 + x) + '0' + y + String.fromCharCode(65 + z) + 'D' + '\n');
             }
         }
     }
-}
             
 
